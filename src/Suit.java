df960b1c9bccb0bd85d2414ec73ee85328742868
treysfordays <austinkwok@live.ca>
2017-01-28 01:12:54 -0800

    first commit

---
+++ src/Suit.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Austin on 2017-01-27.
+ */
+public enum Suit {
+
+    CLUB, DIAMOND, SPADE, HEART
+}
---
+++ src/Value.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Austin on 2017-01-27.
+ */
+public enum Value {
+
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QEEUN, KING, ACE
+}
